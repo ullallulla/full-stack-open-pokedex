384ad7b67b8c5b4c2a927f7ccf038c0bce77cb3a
Aleksi Ojansivu <aleksi.ojansivu@gmail.com>
2024-06-22 16:27:17 +0300

    ex 11.12

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
-app.listen(PORT, () => {
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(PORT + 1, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
